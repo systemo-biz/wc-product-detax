bc3d4c65e031fb2189c822c1ae9660159b67587d
Anatoly Yumashev <yumashev@users.noreply.github.com>
2016-03-28 16:12:25 +0500

    Update wc-service-tax.php

--- wc-service-tax.php
+++ wc-service-tax.php
@@ -1,2 +1,17 @@
 <?php
 
+/*
+Plugin Name:       WC Service Tax
+Description:  Таксономии для описания услуг в WooCommerce
+Author:                 Systemo
+Plugin URI:          https://github.com/systemo-biz/wc-service-tax
+Author URI:         http://systemo.biz
+License:              GPL-2.0+
+License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+Text Domain:      wc-service-tax
+Domain Path:      /languages
+Version:               20150825
+*/
+
+include_once('inc/model.php');
+include_once('inc/view.php');
