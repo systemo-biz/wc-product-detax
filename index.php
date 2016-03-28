5aae7bbcb0fde60b8f692d9ded7111af923394f8
Anatoly Yumashev <yumashev@users.noreply.github.com>
2016-03-28 17:01:38 +0500

    Update and rename wc-service-tax.php to index.php

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
 <?php
 
 /*
-Plugin Name:       WC Service Tax
-Description:  Таксономии для описания услуг в WooCommerce
+Plugin Name:       WC Product Description Taxonomy
+Description:  Таксономии для описания продуктов в WooCommerce
 Author:                 Systemo
-Plugin URI:          https://github.com/systemo-biz/wc-service-tax
+Plugin URI:          https://github.com/systemo-biz/wc-product-detax
 Author URI:         http://systemo.biz
 License:              GPL-2.0+
 License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
 Text Domain:      wc-service-tax
 Domain Path:      /languages
-GitHub Plugin URI: https://github.com/systemo-biz/wc-service-tax
+GitHub Plugin URI: https://github.com/systemo-biz/wc-product-detax
 GitHub Branch: master
 Version: 20160325
 */
